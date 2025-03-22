136370bf64f3f423c4036fbf025842c527730eeb
Rui Chen <rui@chenrui.dev>
2025-03-22 16:01:38 -0400

    remove dup lines
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- backend/data/student_course.sql
+++ backend/data/student_course.sql
@@ -1,24 +1,16 @@
 -- seed data for student_course table
-INSERT INTO student_course
-  (student_id, course_id, semester, grade)
-VALUES
-  (1, 1, 'Fall', 'A'),
-  (1, 2, 'Spring', 'B'),
-  (2, 1, 'Fall', 'C'),
-  (2, 3, 'Spring', 'A'),
-  (3, 2, 'Fall', 'B'),
-  (3, 4, 'Spring', 'A'),
-  (4, 5, 'Fall', 'C'),
-  (5, 6, 'Spring', 'B'),
-  (6, 1, 'Fall', 'A'),
-  (1, 3, 'Fall', 'A'),
-  (2, 4, 'Spring', 'C'),
-  (3, 5, 'Fall', 'B'),
-  (4, 6, 'Spring', 'A'),
-  (5, 1, 'Fall', 'C'),
-  (6, 2, 'Spring', 'B'),
-  (2, 4, 'Spring', 'C'),
-  (3, 5, 'Fall', 'B'),
-  (4, 6, 'Spring', 'A'),
-  (5, 1, 'Fall', 'C'),
-  (6, 2, 'Spring', 'B');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (1, 1, 'Fall', 'A');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (1, 2, 'Spring', 'B');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (2, 1, 'Fall', 'C');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (2, 3, 'Spring', 'A');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (3, 2, 'Fall', 'B');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (3, 4, 'Spring', 'A');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (4, 5, 'Fall', 'C');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (5, 6, 'Spring', 'B');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (6, 1, 'Fall', 'A');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (1, 3, 'Fall', 'A');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (2, 4, 'Spring', 'C');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (3, 5, 'Fall', 'B');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (4, 6, 'Spring', 'A');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (5, 1, 'Fall', 'C');
+INSERT INTO student_course (student_id, course_id, semester, grade) VALUES (6, 2, 'Spring', 'B');
