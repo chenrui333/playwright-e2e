4ed0f25bee97354737ca3f3a7d3f8898d6e18301
Rui Chen <rui@chenrui.dev>
2025-03-22 15:58:54 -0400

    do insertion line by line
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- backend/data/student_course.sql
+++ backend/data/student_course.sql
@@ -10,13 +10,13 @@ VALUES
   (3, 4, 'Spring', 'A'),
   (4, 5, 'Fall', 'C'),
   (5, 6, 'Spring', 'B'),
-  (6, 1, 'Fall', 'A');
+  (6, 1, 'Fall', 'A'),
   (1, 3, 'Fall', 'A'),
   (2, 4, 'Spring', 'C'),
   (3, 5, 'Fall', 'B'),
   (4, 6, 'Spring', 'A'),
   (5, 1, 'Fall', 'C'),
-  (6, 2, 'Spring', 'B');
+  (6, 2, 'Spring', 'B'),
   (2, 4, 'Spring', 'C'),
   (3, 5, 'Fall', 'B'),
   (4, 6, 'Spring', 'A'),
